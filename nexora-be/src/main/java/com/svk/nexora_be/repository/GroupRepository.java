d2d8af4e661302e0a7ecd367a1de1052f8688ea1
shyamvilaskawale <shyamvilaskawale@gmail.com>
2026-04-26 00:33:55 +0530

    updated GroupRepository

--- nexora-be/src/main/java/com/svk/nexora_be/repository/GroupRepository.java
+++ nexora-be/src/main/java/com/svk/nexora_be/repository/GroupRepository.java
@@ -9,5 +9,4 @@ import java.util.Optional;
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
     Optional<Group> findByPublicId(String publicId);
-    Optional<Group> findById(Long id);
 }
