d58480ba09ba4591e5bee323caeb99b983b04265
shyamvilaskawale <shyamvilaskawale@gmail.com>
2026-04-25 23:43:03 +0530

    Added Cache Manager in Redis Config

--- nexora-be/src/main/java/com/svk/nexora_be/config/RedisConfig.java
+++ nexora-be/src/main/java/com/svk/nexora_be/config/RedisConfig.java
@@ -1,10 +1,16 @@
 package com.svk.nexora_be.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -24,6 +30,28 @@ public class RedisConfig {
 
         template.afterPropertiesSet();
         return template;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisSerializer<String> keySerializer = RedisSerializer.string();
+        RedisSerializer<Object> valueSerializer = RedisSerializer.json();
+
+        RedisSerializationContext.SerializationPair<String> keyPair =
+                RedisSerializationContext.SerializationPair.fromSerializer(keySerializer);
+        RedisSerializationContext.SerializationPair<Object> valuePair =
+                RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer);
+
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(keyPair)
+                .serializeValuesWith(valuePair)
+                // default TTL for caches (adjust as needed)
+                .entryTtl(Duration.ofHours(1));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(cacheConfig)
+                .transactionAware()
+                .build();
     }
 }
 
