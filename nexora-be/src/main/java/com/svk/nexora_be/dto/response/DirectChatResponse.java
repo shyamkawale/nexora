ed8b5af503ffd9d27be61a7a4085592efc53afd7
shyamkawale <shyamvilaskawale@gmail.com>
2026-05-17 10:04:06 +0530

    renamed file to make it more consistant

--- nexora-be/src/main/java/com/svk/nexora_be/dto/response/DirectChatResponse.java
+++ nexora-be/src/main/java/com/svk/nexora_be/dto/response/DirectChatResponse.java
@@ -1,5 +1,6 @@
 package com.svk.nexora_be.dto.response;
 
+import com.svk.nexora_be.entity.DirectChat;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,10 +13,19 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class DirectMessageChatResponse {
+public class DirectChatResponse {
     private String publicId;
     private UserResponse user1;
     private UserResponse user2;
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime createdAt;
+
+    public static DirectChatResponse mapDirectChatToResponse(DirectChat chat) {
+        return DirectChatResponse.builder()
+                .publicId(chat.getPublicId())
+                .user1(UserResponse.fromUser(chat.getUser1()))
+                .user2(UserResponse.fromUser(chat.getUser2()))
+                .createdAt(chat.getCreatedAt())
+                .build();
+    }
 }
